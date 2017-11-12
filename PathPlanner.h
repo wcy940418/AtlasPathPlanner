2c7fc4a7fc40f2ad10f4ffb775ad708db1f0de44
szfck <kchen9530@gmail.com>
2017-11-12 15:50:46 -0500

    Add opencv draw

--- PathPlanner.h
+++ PathPlanner.h
@@ -48,8 +48,8 @@ public:
   }
 
   // use SPFA Algorithm as path planning
-  void PathPlanning() {
-      vector<vector<vector<int>>> dis(4, vector<vector<int>>(row + 1, vector<int>(col + 1, INT_MAX)));
+  vector<State> PathPlanning() {
+      vector<vector<vector<int> > > dis(4, vector<vector<int> >(row + 1, vector<int>(col + 1, INT_MAX)));
       map<State, int> vis;
       map<State, State> pre;
       queue<State> que;
@@ -73,7 +73,9 @@ public:
               int dx = x + dir[i][0];
               int dy = y + dir[i][1];
               if (!IsValid(dx, dy)) continue;
+
               int cost = dis[d][x][y] + stepWeight + (i == d ? 0 : directionWeight);
+
               if (cost < dis[i][dx][dy]) {
                   dis[i][dx][dy] = cost;
                   State next = {{dx, dy}, i}; 
@@ -93,19 +95,20 @@ public:
               now = {{destX, destY}, i};
           }
       }
+      paths.clear();
       if (res == INT_MAX) {
           cout << "can not find the path!" << endl;
       } else {
           cout << "find a path, cost is " << res << endl;
-          paths.clear();
           while (now.second != -1) {
             paths.push_back(now);
             now = pre[now];
           }
           reverse(paths.begin(), paths.end());
       }
+      return paths;
   }
-  void showGraph(bool path = false) {
+  void ShowGraph(bool path = false) {
       if (path) {
           for (auto p : paths) {
               graph[p.first.first][p.first.second] = 2;
@@ -138,7 +141,7 @@ public:
 private:
   int row, col;
   vector<State> paths;
-  vector<vector<int>> graph;
+  vector<vector<int> > graph;
   int startX, startY, destX, destY;
   int stepWeight, directionWeight;
   int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
@@ -147,74 +150,4 @@ private:
       return x > 0 && x <= row && y > 0 && y <= col && graph[x][y] == 0;
   }
 };
-class Test {
-public:
-    void case1() {
-        cout << "case 1 : row: " << 5 << " col: " << 5 << endl;
-        PathPlanner plan(5, 5);
-        plan.SetStart(1, 1);
-        plan.SetDestination(5, 5);
-        plan.SetBarrier(3, 1);
-        plan.SetBarrier(1, 5);
-        plan.SetBarrier(2, 5);
-        plan.showGraph();
-        plan.PathPlanning();
-        plan.showGraph(true);
-        cout << endl;
-    }
-
-
-    void case2() {
-        cout << "case 2 : row: " << 10 << " col: " << 10 << endl;
-        PathPlanner plan(10, 10);
-        plan.SetStart(1, 1);
-        plan.SetDestination(10, 1);
-        plan.SetBarrier(2, 1);
-        for (int i = 2; i < 10; i++) {
-            plan.SetBarrier(i, i);
-        }
-        plan.showGraph();
-        plan.PathPlanning();
-        plan.showGraph(true);
-        cout << endl;
-    }
-
-    void case3() {
-        cout << "case 3 : row: " << 10 << " col: " << 10 << endl;
-        PathPlanner plan(10, 10);
-        for (int i = 2; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                plan.SetBarrier(i, j);
-            }
-        }
-        for (int i = 2; i <=6; i++) {
-            plan.SetEmpty(i, 1);
-        }
-        for (int i = 1; i <= 9; i++) {
-            plan.SetEmpty(6, i);
-        }
-        for (int i = 7; i <= 9; i++) {
-            plan.SetEmpty(i, 5);
-        }
-        plan.SetStart(1, 1);
-        plan.SetDestination(10, 1);
-        cout << "set step weight " << 1 << 
-            ", set directionWeight " << 100 << endl;
-        plan.SetStepAndDirectionWeight(1, 100);
-        plan.showGraph();
-        plan.PathPlanning();
-        plan.showGraph(true);
-        cout << endl;
-    }
-};
-int main () {
-    cout << "Graph: S means start, D means destination, . means empty block, # means barrier, * means the suggested path" << endl << endl;
-    Test test;
-    test.case1();
-    test.case2();
-    test.case3();
-
-    return 0;
-}
-
 
